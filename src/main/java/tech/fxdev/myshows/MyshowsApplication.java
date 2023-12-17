9760f2625f0649a6de88ce1f47907326f69ece5d
Dmitrii <d.remiannikov@fxdev.tech>
2023-12-17 15:46:20 +0100

    Initial commit

---
+++ src/main/java/tech/fxdev/myshows/MyshowsApplication.java
@@ -0,0 +1,13 @@
+package tech.fxdev.myshows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyshowsApplication.class, args);
+	}
+
+}
